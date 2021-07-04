f3ea19c944f58962395de28eec8b12ce2c0cc8f6
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-07-04 17:55:10 -0300

    Mi primer commit  con footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./components/General/Footer";
+import './Sass/app.css';
+
+
+function App() {
+  return (
+    <>
+      <Footer pieDePagina="Copyright &copy;, derechos reservados Drone.Co -Junio 2021" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/General/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = (props) => {
+  return (
+    <div className="main-footer">
+      <footer className="footer-title">{props.pieDePagina}</footer>
+    </div>
+  );
+};
+
+export default Footer;
